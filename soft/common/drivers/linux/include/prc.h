f2d72071090f1ac21db27609f70cf9f4b7f52576
yerbic <yerbic@pm.me>
2022-05-01 14:19:19 -0400

    added wip driver

--- soft/common/drivers/linux/include/prc.h
+++ soft/common/drivers/linux/include/prc.h
@@ -4,6 +4,21 @@
 #ifdef __KERNEL__          
 #include <linux/ioctl.h>   
 #include <linux/types.h>   
+#include <linux/of_device.h>
+#include <linux/mm.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/errno.h>
+#include <linux/version.h>
+#include <linux/kernel.h>
+#include <linux/platform_device.h>
+#include <linux/miscdevice.h>
+#include <linux/slab.h>
+#include <linux/io.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
 #else                      
 #include <sys/ioctl.h>     
 #include <stdint.h>        
@@ -15,7 +30,7 @@
 #include <esp.h>           
 
 #define PRC_MAGIC 		'P'
-#define PRC_BASE_ADDR_OFFSET 	0xE400
+#define PRC_OFFSET		0xE400
 #define PRC_START		0x1
 #define PRC_STOP		0x0
 #define MONITOR_BASE_ADDR 	0x90180
--- soft/common/drivers/linux/prc/prc.c
+++ soft/common/drivers/linux/prc/prc.c
@@ -3,34 +3,21 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <esp.h>
+#include "prc.h"
+#define DRV_NAME	"prc"
 
-#include <linux/of_device.h>
-#include <linux/mm.h>
-
-#include <linux/module.h>
-#include <linux/init.h>
-#include <linux/errno.h>
-#include <linux/version.h>
-#include <linux/kernel.h>
-#include <linux/platform_device.h>
-#include <linux/miscdevice.h>
-#include <linux/slab.h>
-#include <linux/io.h>
-#include <linux/of.h>
-#include <linux/of_address.h>
-#include <linux/fs.h>
-#include <linux/uaccess.h>
-
-struct prc_device {
-	struct esp_device esp;
-}
+struct esp_prc_device {
+	struct device 	*dev;
+	struct resource res;
+	struct module 	*module;
+	void iomem 		*iomem;
+} prc_dev;
 
 static struct esp_driver prc_driver;
 static struct prc_device prc_dev;
 static struct pbs_map *pb_map;
 
-static struct of_device_id <accelerator_name>_device_ids[] = {
+static struct of_device_id esp_prc_device_ids[] = {
 	{
 		.name = "SLD_PRC",
 	},
@@ -41,19 +28,53 @@ static struct of_device_id <accelerator_name>_device_ids[] = {
 		.compatible = "sld,prc",
 	},
 	{ },
+};
 
-static inline struct prc_device *to_prc(struct esp_device *esp)
+
+static inline int get_decoupler_addr(struct esp_device *dev, struct esp_device *decoupler)
 {
-	return container_of(esp, struct prc_device, esp);
+	unsigned i;
+	unsigned tile_id = 0xFF;
+	unsigned dev_addr;
+	unsigned dev_addr_trunc;
+	unsigned dev_start_addr = 0x10000;
+
+	const unsigned addr_incr = 0x100;
+	const unsigned monitor_base = 0x90180;
+
+	dev_addr = (unsigned) dev->addr;
+	dev_addr_trunc = (dev_addr << 12) >> 12;
+	//printf("device address %0x truncated addr %0x \n", dev_addr, dev_addr_trunc);
+	//#ifdef ACCS_PRESENT
+	//Obtain tile id
+	for (i = 0; i < SOC_NACC; i++) {
+		if(dev_start_addr == dev_addr_trunc) {
+			tile_id = acc_locs[i].row * SOC_COLS + acc_locs[i].col;
+			break;
+		}
+		else
+			dev_start_addr += addr_incr;
+	}
+
+	if(tile_id == 0XFF) {
+		fprintf(stderr, "Error: cannot find tile id\n");
+		exit(EXIT_FAILURE);
+	}
+
+	//compute apb address for tile decoupler
+	(*decoupler).addr = APB_BASE_ADDR + (monitor_base + tile_id * 0x200);
+	//printf("tile_id is %0x decoupler addr is %0x \n", tile_id, (unsigned) esp_tile_decoupler.addr);
+	return 0;
 }
 
 int decouple_acc(struct esp_device *dev, unsigned val)
 {
+	struct esp_device esp_tile_decoupler;
 	get_decoupler_addr(dev, &esp_tile_decoupler);
 	if (val == 0)
-		iowrite32(&esp_tile_decoupler, 0, 0);
-	else 
-		iowrite32(&esp_tile_decoupler, 0, BIT(0));
+		iowrite32(0, prc_dev.iomem);
+	else
+		iowrite32(1, prc_dev.iomem);
 
 	return 0;
 }
@@ -93,11 +114,20 @@ static void stop(struct esp_device *esp, void *arg)
 	return 0;
 }
 
-static void reconfigure(struct esp_device *esp, void *arg)
+static void init_prc()
+{
+    esp_prc.addr = (long long unsigned) APB_BASE_ADDR + PRC_OFFSET;
+
+    pb_map = (struct pbs_map *) &bs_descriptor;
+
+    //printf("bitstream addr %0x %08x \n", pb_map->pbs_size, (unsigned) pb_map->pbs_addr);
+}
+
+static void reconfigure(void)
 {
 	//   int status = 0;
 
-	init_prc();
+	//init_prc();
 	set_trigger(pbs_id);
 
 	if(!(start_prc())) {
@@ -111,91 +141,104 @@ static void reconfigure(struct esp_device *esp, void *arg)
 		exit(EXIT_FAILURE);
 	}
 	/*
-	   while(!status){
-	   status = ioread32(&esp_prc, 0x0);
-	   status &= (1 << 2);
-	   }
-	   */
+	while(!status){
+	status = ioread32(&esp_prc, 0x0);
+	status &= (1 << 2);
+	}
+	*/
 	printf("PRC: Reconfigured FPGA \n");
 }
 
-static void set_trigger(struct esp_device *esp, void *arg)
+static void set_trigger(int pbs_id)
 {
 	if (!shutdown_prc()) {
 		iowrite32(&esp_prc, 0x60, 0x0);
 		iowrite32(&esp_prc, 0x64, PBS_BASE_ADDR + pb_map[pbs_id].pbs_addr);
 		iowrite32(&esp_prc, 0x68, pb_map[pbs_id].pbs_size);
 		printf("PRC: Trigger armed \n");
-	} else 
+	} else
 		printf("PRC: Error arming trigger \n");
 }
 
-static void prc_prep_xfer(struct esp_device *esp, void *arg)
+static long esp_prc_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
 {
-	return;
+	switch (cmd) {
+		case PRC_START:
+			break;
+		case PRC_STOP:
+			break;
+		case PRC_SET_TRIGGER:
+			break;
+		case PRC_RECONFIGURE:
+			break;
 }
 
-static bool prc_xfer_input_ok(struct esp_device *esp, void *arg)
+static const struct file_operations esp_prc_fops = {
+	.owner 		= THIS_MODULE,
+	.unlocked_ioctl = esp_prc_ioctl,
+};
+
+static struct miscdevice esp_prc_misc_device = {
+	.minor 	= MISC_DYNAMIC_MINOR,
+	.name 	= DRV_NAME,
+	.fops	= esp_prc_fops,
+};
+
+static int esp_prc_probe(struct platform_device *pdev)
 {
-	return true;
-}
+	int ret;
 
-static int __exit prc_remove(struct platform_deivce *pdev)
-{
-}
+	ret = misc_register(&esp_prc_misc_device);
+	ret = of_address_to_resource(pdev->dev.of_node, 0, &prc_dev.res);
+	if (ret) {
+		ret = -ENOENT;
+		goto deregister_res;
+	}
 
-static int prc_probe(struct platform_deivce *pdev)
-{
-	struct prc_device *prc;
-	struct esp_device *esp;
-	int rc;
+	if (request_mem_region(prc_dev.res.start, resource_size(&prc_dev.res),
+				DRV_NAME) == NULL)
+	{
+		ret = -EBUSY;
+		goto deregister_res;
+	}
 
-	prc = kzalloc(sizeof(*prc), GFP_KERNEL);
-	if (prc == NULL)
-		return -ENOMEM;
-	esp = &prc->esp;
-	esp->module = THIS_MODULE;
-	esp->number = 0;
-	esp->driver = prc_driver;
-	esp->addr = (long long unsigned) APB_BASE_ADDR + 0xE400;
-	pb_map = (struct pbs_map *) &bs_descriptor;
+	prc_dev.iomem = of_iomap(pdev->dev.of_node, 0);
+	if (prc_dev.iomem == NULL) {
+		ret = -ENOMEM;
+		goto release_mem;
+	}
 
-	rc = esp_deivce_register(esp, pdev);
-	if (rc)
-		goto err;
 	return 0;
-err:
-	kfree(mac);
-	return rc;
 
+release_mem:
+	release_mem_region(prc_dev.res.start, resource_size(&prc_dev.res));
+deregister_res:
+	misc_deregister(&esp_prc_misc_device);
+	return ret;
 }
 
-static struct esp_driver prc_driver = {
-	.plat = {
-		.probe		= prc_probe,
-		.remove		= prc_probe,
+static int __exit esp_prc_remove(struct platform_deivce *pdev)
+{
+	iounmap(prc_dev.iomem);
+	release_mem_region(prc_dev.res.start, resource_size(&prc_dev.res));
+	misc_deregister(&esp_prc_misc_device);
+	return 0;
+}
+
+static struct platform_driver esp_prc_driver = {
+		.remove		= esp_prc_remove,
 		.driver		= {
 			.name 		= DRV_NAME,
 			.owner		= THIS_MODULE,
-			.of_match_table	= prc_device_id,
+			.of_match_table	= esp_prc_device_ids,
 		},
-	},
-	.xfer_input_ok	= prc_xfer_input_ok,
-	.prep_xfer 	= prc_prep_xfer,
-	.ioctl_cm	= LIST_HEAD(ioctl_cm_list),
-	.arg_size	= sizeof(struct prc_access),
 };
 
-static struct esp_ioctl_cm set_trigger_cm = {
-	.list INIT_LIST_HEAD(prc_driver.ioctl_cm.list),
-	.cm = PRC_SET_TRIGGER,
-	.handle = set_trigger;
-};
 
-list_add(<accelerator_name>_driver.ioctl_cm, <accelerator_name>_ioctl_cm);
-static int __init prc_init(void)
+static int __init esp_prc_init(void)
 {
-	return esp_driver_register(&esp_driver);
+	pr_info(DRV_NAME ": init\n");
+	return platform_driver_probe(&esp_prc_driver, esp_prc_probe);
 }
 
 static int __exit prc_exit(void)
@@ -203,9 +246,11 @@ static int __exit prc_exit(void)
 	return esp_driver_unregister(&esp_driver);
 }
 
-module_init(prc_init);
-module_init(prc_exit);
+module_init(esp_prc_init);
+module_exit(esp_prc_exit);
+
+MODULE_DEVICE_TABLE(of, esp_prc_device_ids);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Bryce Natter <brycenatter@pm.me");
-MODULE_DESCRIPTION("PRC driver");
+MODULE_DESCRIPTION("esp PRC driver");
